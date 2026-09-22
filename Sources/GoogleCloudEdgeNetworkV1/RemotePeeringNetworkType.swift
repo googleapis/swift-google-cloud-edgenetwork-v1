14a51f67f610da6269944cda419b6c6e810236d5
Carlos O'Ryan <coryan@google.com>
2026-09-21 20:42:28 -0400

    chore: update librarian w/ better enum docs (#1017)
    In this version of librarian we improve the generated documentation for
    `enum` so the possible evolution and usage of the enums is more obvious.
    For example, we recommend using `@unknown default:` so they get warnings
    when new values appear in the enum.

--- Sources/GoogleCloudEdgeNetworkV1/RemotePeeringNetworkType.swift
+++ Sources/GoogleCloudEdgeNetworkV1/RemotePeeringNetworkType.swift
@@ -18,6 +18,12 @@ import Foundation
 
 /// Defines the remote peering destination for the interface. It is required
 /// when peering separation is enabled.
+///
+/// - Note: Adding cases to this enumeration is not considered a breaking change.
+///   Always include an `@unknown default:` case when switching over this type.
+///   Do not pattern-match against `unknownStringValue` or `unknownIntValue`
+///   expecting specific values to remain unparsed; future releases may promote
+///   them to named cases.
 public enum RemotePeeringNetworkType: Codable, Equatable, Sendable {
   /// Unspecified.
   case unspecified
@@ -27,15 +33,21 @@ public enum RemotePeeringNetworkType: Codable, Equatable, Sendable {
   case customerInternet
   /// Encodes an unknown integer value.
   ///
-  /// The most common cause for an unknown values is for the service to send
+  /// The most common cause for an unknown value is for the service to send
   /// a value unknown to the library. We recommend you update your library to
   /// the latest version.
+  ///
+  /// - Warning: Do not pattern-match specific integer values in this case;
+  ///   future releases may promote them to named enum cases.
   case unknownIntValue(Int)
   /// Encodes an unknown string value.
   ///
-  /// The most common cause for an unknown values is for the service to send
+  /// The most common cause for an unknown value is for the service to send
   /// a value unknown to the library. We recommend you update your library to
   /// the latest version.
+  ///
+  /// - Warning: Do not pattern-match specific string literals in this case;
+  ///   future releases may promote them to named enum cases.
   case unknownStringValue(String)
 
   public init() {
--- Sources/GoogleCloudEdgeNetworkV1/ResourceState.swift
+++ Sources/GoogleCloudEdgeNetworkV1/ResourceState.swift
@@ -21,6 +21,12 @@ import Foundation
 /// PROVISIONING -> RUNNING. A normal lifecycle of an existing resource being
 /// deleted would be: RUNNING -> DELETING. Any failures during processing will
 /// result the resource to be in a SUSPENDED state.
+///
+/// - Note: Adding cases to this enumeration is not considered a breaking change.
+///   Always include an `@unknown default:` case when switching over this type.
+///   Do not pattern-match against `unknownStringValue` or `unknownIntValue`
+///   expecting specific values to remain unparsed; future releases may promote
+///   them to named cases.
 public enum ResourceState: Codable, Equatable, Sendable {
   /// Unspecified state.
   case stateUnknown
@@ -36,15 +42,21 @@ public enum ResourceState: Codable, Equatable, Sendable {
   case stateDeleting
   /// Encodes an unknown integer value.
   ///
-  /// The most common cause for an unknown values is for the service to send
+  /// The most common cause for an unknown value is for the service to send
   /// a value unknown to the library. We recommend you update your library to
   /// the latest version.
+  ///
+  /// - Warning: Do not pattern-match specific integer values in this case;
+  ///   future releases may promote them to named enum cases.
   case unknownIntValue(Int)
   /// Encodes an unknown string value.
   ///
-  /// The most common cause for an unknown values is for the service to send
+  /// The most common cause for an unknown value is for the service to send
   /// a value unknown to the library. We recommend you update your library to
   /// the latest version.
+  ///
+  /// - Warning: Do not pattern-match specific string literals in this case;
+  ///   future releases may promote them to named enum cases.
   case unknownStringValue(String)
 
   public init() {
